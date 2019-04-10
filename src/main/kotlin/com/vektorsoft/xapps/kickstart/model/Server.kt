ee10dea05e22cc80cec9c291407d2e708e329de4
Vladimir Djurovic <vdjurovic@vektorsoft.com>
2019-04-10 14:35:14 +0200

    changed license header, removed Jackson XML dependency

--- src/main/kotlin/com/vektorsoft/xapps/kickstart/model/Server.kt
+++ src/main/kotlin/com/vektorsoft/xapps/kickstart/model/Server.kt
@@ -1,25 +1,13 @@
 /*
- * Copyright (c) 2018. Vladimir Djurovic
+ * Copyright (c) 2019. Vladimir Djurovic
  *
- * This file is part of Kickstart.
- *
- * Kickstart is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Kickstart is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Kickstart.  If not, see <https://www.gnu.org/licenses/>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 package com.vektorsoft.xapps.kickstart.model
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 
-data class Server(@JacksonXmlProperty(isAttribute = true, localName = "base-url") val baseUrl : String = "") {
+data class Server( val baseUrl : String = "") {
 }
--- src/test/kotlin/com/vektorsoft/xapps/model/DeploymentConfigTest.kt
+++ src/test/kotlin/com/vektorsoft/xapps/model/DeploymentConfigTest.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019. Vladimir Djurovic
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.vektorsoft.xapps.model
 
 import com.vektorsoft.xapps.kickstart.model.DeploymentDescriptor
